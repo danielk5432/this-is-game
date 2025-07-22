a8d4fb460b62da6404bd9caa313f756d26bf39e3
danielk <danielk5432@gmail.com>
2025-07-22 16:42:38 +0900

    tutorial level 1 complete

--- Week3/Assets/Script/Level/BaseLevelController.cs
+++ Week3/Assets/Script/Level/BaseLevelController.cs
@@ -1,33 +1,110 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public abstract class BaseLevelController : MonoBehaviour
 {
+    // A singleton instance to be accessible from other scripts like machines.
+    public static BaseLevelController Instance { get; private set; }
+
+    protected enum LevelState { Preparing, Playing, Cleared }
+    protected LevelState currentState;
+
     [Header("Base Level Settings")]
-    public PlayerSpawner playerSpawner; // 플레이어 스포너 참조
+    [Tooltip("A unique ID for this level, e.g., 'Tutorial_1' or 'Forest_Main'")]
+    public string levelId;
+    [Tooltip("Reference to the Player Spawner.")]
+    public PlayerSpawner playerSpawner;
+    [Tooltip("The number of machine repairs required to clear this level.")]
+    [SerializeField] protected int repairsToClear = 6;
+
+    protected int currentRepairs = 0;
+
+    protected virtual void Awake()
+    {
+        // Singleton setup
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        currentState = LevelState.Preparing;
+    }
 
     protected virtual void Start()
     {
-        // 모든 레벨에서 공통으로 실행될 로직
-        SpawnPlayer();
-        SetupCommonUI();
-
-        // 각 레벨별 고유 로직을 실행할 수 있도록 가상 함수 호출
-        InitializeLevel();
-    }
-
-    protected void SpawnPlayer()
-    {
-        if (playerSpawner != null)
+        if (GameDataModel.Instance.IsLevelCleared(levelId))
         {
-            playerSpawner.SpawnPlayer(); // 스포너에게 생성 요청
+            Debug.Log("Level '" + levelId + "' is already cleared. Skipping level start.");
+            // If cleared, just open the doors and do nothing else.
+            OnAlreadyCleared();
+        }
+        // ----------------------
+        else
+        {
+            // If not cleared, start the level normally.
+            currentState = LevelState.Preparing;
+            SpawnPlayer();
+            SetupCommonUI();
+            InitializeLevel();
+            currentState = LevelState.Playing;
         }
     }
 
-    protected void SetupCommonUI()
+    /// <summary>
+    /// Called by machines when they are successfully repaired.
+    /// </summary>
+    public virtual void OnMachineRepaired()
     {
-        // 공통 UI (예: 일시정지 버튼 등) 설정 로직
+        if (currentState != LevelState.Playing) return;
+
+        currentRepairs++;
+        Debug.Log("Repairs: " + currentRepairs + " / " + repairsToClear);
+
+        // Check for level clear condition.
+        if (currentRepairs >= repairsToClear)
+        {
+            StartCoroutine(LevelClearRoutine());
+        }
     }
 
-    // 자식 클래스에서 재정의(override)할 수 있는 가상 함수
+    private IEnumerator LevelClearRoutine()
+    {
+        currentState = LevelState.Cleared;
+        Debug.Log("LEVEL CLEAR!");
+
+        // --- Clear Sequence ---
+        // 1. Stop all machines and enemy spawners. (This will be implemented in child class)
+        GameDataModel.Instance.MarkLevelAsCleared(levelId);
+        OnLevelClear();
+
+        // 2. Visual effects (camera shake, fade to white).
+        // For now, we'll just wait.
+        yield return new WaitForSeconds(1.5f); // Simulate effects duration.
+
+        // 3. Open the door/teleporter to the next level.
+        OpenExit();
+    }
+
+    // --- Methods to be implemented by child classes ---
     protected abstract void InitializeLevel();
+    protected abstract void OnLevelClear();
+    protected abstract void OpenExit();
+    protected virtual void OnAlreadyCleared()
+    {
+        // By default, just open the exit. Child classes can add more logic.
+        OpenExit();
+    }
+
+    // --- Common Methods ---
+    protected void SpawnPlayer()
+    {
+        if (playerSpawner != null) playerSpawner.SpawnPlayer();
+    }
+
+    protected void SetupCommonUI() { /* Common UI setup logic */ }
 }
