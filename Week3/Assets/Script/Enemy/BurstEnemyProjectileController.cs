64c2ace4d760a2db4e52683d3fb8ac07ea2bb490
danielk <danielk5432@gmail.com>
2025-07-22 23:28:58 +0900

    made burstenemy new (same as original)

---
+++ Week3/Assets/Script/Enemy/BurstEnemyProjectileController.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class BurstEnemyProjectileController : MonoBehaviour
+{
+    private Vector2 direction;
+    private float speed;
+
+    public string enemyTag = "Enemy";
+    public string boxTag = "Box";
+    public string playerTag = "Player";
+
+    public float selfDestructTime = 5f;
+
+    public void Initialize(Vector2 dir, float spd)
+    {
+        direction = dir;
+        speed = spd;
+    }
+
+    void Start()
+    {
+        Destroy(gameObject, selfDestructTime);
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(enemyTag))
+        {
+            return; // 무시
+        }
+        else if (other.CompareTag(boxTag))
+        {
+            BaseBox box = other.GetComponent<BaseBox>();
+            if (box != null)
+            {
+                box.DestroyBox();
+            }
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag(playerTag))
+        {
+            PlayerController pc = other.GetComponent<PlayerController>();
+            if (pc != null)
+            {
+                pc.SetStun(2f);
+                pc.DropAllBoxes();
+            }
+            Destroy(gameObject);
+        }
+        else
+        {
+            // 벽이나 기타 오브젝트
+            Destroy(gameObject);
+        }
+    }
+}
