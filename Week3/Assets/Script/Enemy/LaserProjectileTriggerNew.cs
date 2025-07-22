699602311842815c3417b30926ca11c6eaef9fb6
danielk <danielk5432@gmail.com>
2025-07-23 02:12:16 +0900

    enemy finish

---
+++ Week3/Assets/Script/Enemy/LaserProjectileTriggerNew.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class LaserProjectileTriggerNew : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Box"))
+        {
+            BaseBox box = other.GetComponent<BaseBox>();
+            if (box != null)
+            {
+                box.DestroyBox();
+            }
+        }
+        else if (other.CompareTag("Player"))
+        {
+            var player = other.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.DropAllBoxes();
+                player.SetStun(1.5f);
+            }
+        }
+    }
+}
